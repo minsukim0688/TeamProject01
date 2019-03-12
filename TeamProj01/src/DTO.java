9ae4f52e0c6a425b94b2baf2dad6c57eaa82b18c
kosmo_05 <kosmo_05@DESKTOP-8TO7463>
2019-03-12 12:06:47 +0900

    학원에서 두번째 파일생성 및 수정

---
+++ TeamProj01/src/DTO.java
@@ -0,0 +1,14 @@
+
+public class DTO {
+	private String name;
+	public DTO(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
